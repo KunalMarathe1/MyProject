4f0d91fbdd867fa5aa17dcd8ddb2ef70d824c39d
Kunal MArathe <kunalmarathe730@gmail.com>
2022-07-13 17:29:23 +0530

    Complete Program

--- src/starpatternlogical/Eg1.java
+++ src/starpatternlogical/Eg1.java
@@ -2,23 +2,18 @@ package starpatternlogical;
 
 public class Eg1 {
 
-	public static void main(String[] args) {
-		
-		sir("*");
-		
-		
-		
-	}
-	public static void sir(String name)
+	public static void main(String[] args)
 	{
 		for(int i=1;i<=5;i++)
 		{
 			for(int j=1;j<=5;j++)
 			{
-				System.out.print(name);
+				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
+		
+	
 
 }
--- src/starpatternlogical/Eg2.java
+++ src/starpatternlogical/Eg2.java
@@ -21,6 +21,7 @@ public class Eg2
 		}
 	}
 	
+	
 	public static void kunal()
 	{
 		for(int i=1;i<=5;i++)
@@ -33,3 +34,24 @@ public class Eg2
 		}
 	}
 }
+
+//package starpatternlogical;
+//
+//public class Eg1 {
+//
+//	public static void main(String[] args)
+//	{
+//		for(int i=1;i<=5;i++)
+//		{
+//			for(int j=1;j<=5;j++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
+//		
+//	
+// Write this code 
+//}
+
